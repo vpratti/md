7a778e8f369f7be1413aa99d5703123d5fec73fb
sonmays <somays@gannett.com>
2015-02-21 18:48:27 -0500

    changing menu to use dropdown for user management actions

---
+++ AngularJSAuthentication.Web/app/directives.js
@@ -0,0 +1,30 @@
+﻿(function(angular) {
+    'use strict';
+
+    angular
+        .module('VirtualClarityApp')
+        .directive('vcHeaderDropdown', vcHeaderDropdown);
+
+    function vcHeaderDropdown() {
+        var directive = {
+            restrict: 'A',
+            link: linkFunc
+        };
+
+        return directive;
+
+        function linkFunc(scope, element) {
+
+            element.on('click', function () {
+                var parent = element.parent()[0];
+                if (parent.className == "dropdown") {
+                    parent.className += " open";
+                } else {
+                    parent.className = "dropdown";
+                }
+            });
+        }
+    }
+
+
+}(angular));
